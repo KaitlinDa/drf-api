14cd7fdb8ab88d0ad49311bfd8911af3f9a6537b
KaitlinDa <Kaitlin.A.Davis1996@Gmail.com>
2024-02-21 20:35:23 -0600

    adds permission and database

--- drf_api/views.py
+++ drf_api/views.py
@@ -1,6 +1,7 @@
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from .models import Drf_api
 from .serializers import Drf_api_Serializer
+from .permissions import IsOwnerOrReadOnly
 
 class Drf_api_List(ListCreateAPIView):
     queryset = Drf_api.objects.all()
@@ -10,3 +11,4 @@ class Drf_api_List(ListCreateAPIView):
 class Drf_api_Detail(RetrieveUpdateDestroyAPIView):
     queryset = Drf_api.objects.all()
     serializer_class = Drf_api_Serializer
+    permission_classes = (IsOwnerOrReadOnly,)
--- drf_api_project/settings.py
+++ drf_api_project/settings.py
@@ -81,13 +81,23 @@ WSGI_APPLICATION = 'drf_api_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "postgres",
+        "USER": "postgres",
+        "PASSWORD": "password",
+        "HOST": "db",
+        "PORT": 5432, 
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
@@ -129,3 +139,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    "DEFAULT_PERMISSION-CLASSES": [
+        "rest_framework.permissions.IsAuthenticated",
+    ]
+}
